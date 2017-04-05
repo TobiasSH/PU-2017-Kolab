a8eb0901564a4b76c4db370333b61325a549221d
Thodnebo <thodnebo@gmail.com>
2017-04-05 14:58:13 +0200

    New attempts at binding hostname to python

--- nlp.py
+++ nlp.py
@@ -167,7 +167,7 @@ class Namespace(BaseNamespace):
         processNew(message)
 
 print ("Connecting to socketIO.. Port is: ", port['text'])
-socketIO = SocketIO('kolab-group.herokuapp.com', port['text'], Namespace) #connects to localhost:3000
+socketIO = SocketIO('129.241.228.50', port['text'], Namespace) #connects to localhost:3000
 socketIO.wait() #waits forever
 
 
